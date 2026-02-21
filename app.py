a23182f485c86d6b02bfc65ef6dd1b9673fcd6b3
shreyaskulkarni33 <45578102+shreyaskulkarni33@users.noreply.github.com>
2026-02-21 14:31:46 -0800

    Update app.py
    Update initial comment to comment in code only.

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ import numpy as np
 import streamlit as st
 from openai import OpenAI
 
-''' 
-Here, we will leverage the streamlit package to set up a title, layout, caption and add an emoji as well. 
 
-''' 
+#Here, we will leverage the streamlit package to set up a title, layout, caption and add an emoji as well. 
+
 st.set_page_config(page_title="NBA Lineup Chatbot", layout="wide")
 st.title("🏀 NBA Lineup Analytics Chatbot")
 st.markdown(
